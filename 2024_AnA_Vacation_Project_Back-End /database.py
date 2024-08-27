224ed5686281a779d3b5beb2af04c96d949225a4
Jeong Jihyo <jihyo0331@gmail.com>
2024-08-27 23:03:08 +0900

    a

--- 2024_AnA_Vacation_Project_Back-End /database.py
+++ 2024_AnA_Vacation_Project_Back-End /database.py
@@ -3,8 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
-
-
 load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
